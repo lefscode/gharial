9fa94997a76ad804c01447f532b8532d0f164e38
lefscode <eleftherios.lampros@intrasoft-intl.com>
2018-02-26 14:45:12 +0200

    added clara.hpp for command line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,43 @@
 #include "controller.hpp"
+#include "clara.hpp"
 
 using namespace std;
 using namespace Pistache;
-
+using namespace clara;
 
 int main(int argc, char *argv[]) {
-    
-    Port port(9912);
 
-    int thr = 2;
+    int port_val = -1;
+    int thread_count = -1;
+    auto cli
+        = Opt( port_val, "port" )
+            ["-p"]["--port"]
+            ("How wide should it be?")
+        | Opt( thread_count, "threads" )
+            ["-t"]["--threads"]
+            ("By what name should I be known");
 
-    if (argc >= 2) {
-        port = std::stol(argv[1]);
-
-        if (argc == 3)
-            thr = std::stol(argv[2]);
+    auto result = cli.parse( Args( argc, argv ) );
+    if( !result ) {
+        std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
+        exit(1);
     }
+
+    if ( port_val == -1 )
+        port_val = 9912;
+
+    if ( thread_count == -1 )
+        thread_count = 2;
+
+    std::cout << "Starting Server at port: " << port_val << " using: " << thread_count << " thread(s)\n";
+
+    Port port(port_val);
 
     Address a(Ipv4::any(), port);
 
     Controller c(a);
 
-    c.initialize(thr);
+    c.initialize(thread_count);
     c.start();
 
     c.shutdown();
